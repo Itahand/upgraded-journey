73cf872b0cb196137fed6ece9fb9eeec296e617f
Itahand Naizir <itahand.naizir@gmail.com>
2022-07-04 15:50:19 -0500

    Navbar centered.

--- upgraded-journey/src/App.svelte
+++ upgraded-journey/src/App.svelte
@@ -2,24 +2,23 @@
 	export let names = 'Itahand Naizir';
 </script>
 
-<header class="navbar">
-	<nav>
-		<ul>
-			<li>
-				Profile Picture
-			</li>
-			<li>
-				About
-			</li>
-			<li>
-				Experience
-			</li>
-		</ul>
-	</nav>
-</header>
 <main>
+	<header class="header">
+		<nav class="navbar">
+			<ul>
+				<li>
+					<img src="https://i.postimg.cc/Y0gQ47Lt/profile-pic.jpg" alt="profile" class="profile">
+				</li>
+				<li>About</li>
+				<li>Experience</li>
+				<li>Education</li>
+				<li>Skills</li>
+				<li>Portfolio</li>
+				<li>Interests</li>
+			</ul>
+		</nav>
+	</header>
 	<h1>Hello {names}! </h1>
-
 </main>
 
 <style>
@@ -29,22 +28,32 @@
 		max-width: 240px;
 		margin: 0 auto;
 	}
-
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
 		font-weight: 100;
 	}
 
-	.navbar {
+	.header {
 		position: fixed;
 		background: palevioletred;
 		height: 100%;
-		width: 200px;
+		width: 250px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
 	}
-	.navbar ul {
+	.header ul {
 		padding: 1rem;
 		list-style: none;
+	}
+	.navbar {
+		position: relative;
+	}
+	.profile {
+		height: 150px;
+		width: 150px;
+		border-radius: 50%;
 	}
 
 	@media (min-width: 640px) {
