ae9a99dfcfba7feacfeb11f791768aa7b5c22031
Itahand Naizir <itahand.naizir@gmail.com>
2022-07-04 14:19:31 -0500

    Re-starting with navbar.

--- upgraded-journey/src/App.svelte
+++ upgraded-journey/src/App.svelte
@@ -1,16 +1,31 @@
-<script lang="ts">
-	export let names: string = 'Clark Kent';
+<script>
+	export let names = 'Itahand Naizir';
 </script>
 
+<header class="navbar">
+	<nav>
+		<ul>
+			<li>
+				Profile Picture
+			</li>
+			<li>
+				About
+			</li>
+			<li>
+				Experience
+			</li>
+		</ul>
+	</nav>
+</header>
 <main>
-	<h1>Hello {names}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello {names}! </h1>
+
 </main>
 
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
+		padding: 0;
 		max-width: 240px;
 		margin: 0 auto;
 	}
@@ -18,8 +33,18 @@
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
-		font-size: 4em;
 		font-weight: 100;
+	}
+
+	.navbar {
+		position: fixed;
+		background: palevioletred;
+		height: 100%;
+		width: 200px;
+	}
+	.navbar ul {
+		padding: 1rem;
+		list-style: none;
 	}
 
 	@media (min-width: 640px) {
